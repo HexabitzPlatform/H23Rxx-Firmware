63c026483f55e172fe1f179b2cbabdb7f65704e3
Najibkassab <najeebkassab@hotmail.com>
2020-07-05 22:16:36 +0300

    it’s a release candidate 0.2.2

--- User/topology_1.h
+++ User/topology_1.h
@@ -2,13 +2,13 @@
     BitzOS (BOS) V0.0.0 - Copyright (C) 2016 Hexabitz
     All rights reserved
 
-    File Name     : topology_ex.h
+    File Name     : topology_1.h
     Description   : Array topology definition.
 */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __topology_ex_H
-#define __topology_ex_H
+#ifndef __topology_1_H
+#define __topology_1_H
 #ifdef __cplusplus
  extern "C" {
 #endif
@@ -17,23 +17,22 @@
 #include "stm32f0xx_hal.h"
 	 
 
-#define _N	2					// Number of array modules
+#define __N	2				// Number of array modules
 
 // Array modules
 #define _mod1	1<<3
 #define _mod2	2<<3
 
 
-
 // Topology
-static uint16_t array[_N][7] = {
-{ _H23R1, _mod2|P5, 0, 0,0,0 , 0},		// Module 1
-{ _H01R0, 0, 0, 0,0 , _mod1|P1, 0},		// Module 2
+static uint16_t array[__N][7] = {
+{ _H01R0, 0, 0, 0, _mod2|P5, 0, _mod3|P4},									// Module 1
+{ _H01R0, 0, 0, _mod4|P6, 0, _mod1|P4, 0},									// Module 2
 };
 
 // Configurations for duplex serial ports
 #if ( _module == 1 )
-	#define	H23R1	1
+	#define	H0BR4	1
 	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
@@ -51,11 +50,10 @@ static uint16_t array[_N][7] = {
 	#define	_P6pol_normal	1
 #endif
 
-
 #ifdef __cplusplus
 }
 #endif
-#endif /*__ topology_ex_H */
+#endif /*__ topology_1_H */
 
 
 /************************ (C) COPYRIGHT HEXABITZ *****END OF FILE****/
