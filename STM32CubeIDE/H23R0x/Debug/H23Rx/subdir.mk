b4d53a3b6bc2000464f806dc66c53a60406434c6
Najibkassab <najeebkassab@hotmail.com>
2021-03-03 14:31:38 +0200

    delete uVision folder and add .hex files to compiled folder

---
+++ STM32CubeIDE/H23R0x/Debug/H23Rx/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+O_SRCS += \
+../Debug/H23Rx/H23Rx.o \
+../Debug/H23Rx/H23Rx_dma.o \
+../Debug/H23Rx/H23Rx_gpio.o \
+../Debug/H23Rx/H23Rx_it.o \
+../Debug/H23Rx/H23Rx_rtc.o \
+../Debug/H23Rx/H23Rx_timers.o \
+../Debug/H23Rx/H23Rx_uart.o \
+../Debug/H23Rx/startup_stm32f091xc.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+
